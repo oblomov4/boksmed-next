1fc3315f27cb74d0c4a37275842e159f149c5966
oblomov4 <vlad.oblomov4@gmail.com>
2025-04-22 20:46:49 +0300

    fonts added

---
+++ shared/fonts/index.ts
@@ -0,0 +1,98 @@
+import localFont from 'next/font/local';
+
+export const geometria = localFont({
+  src: [
+    {
+      path: './Geometria-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './Geometria-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './Geometria-ExtraBoldItalic.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+
+    {
+      path: './Geometria-ExtraLightItalic.woff2',
+      weight: '200',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './Geometria-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-Heavy.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+
+    {
+      path: './Geometria-HeavyItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+
+    {
+      path: './Geometria-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+
+    {
+      path: './Geometria-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+
+    {
+      path: './Geometria-Thin.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+
+    {
+      path: './Geometria-ThinItalic.woff2',
+      weight: '200',
+      style: 'italic',
+    },
+  ],
+});
