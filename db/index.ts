d43364fef483b344be88781e6e8c9d07e5f91450
oblomov4 <vlad.oblomov4@gmail.com>
2025-05-04 05:22:59 +0300

    neon change

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,7 @@
-import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle({ client: sql, schema });
