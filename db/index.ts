a6cf3f4c4ea1664524460ce382458531c1c429c3
oblomov4 <vlad.oblomov4@gmail.com>
2025-05-04 05:07:54 +0300

    neon

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 export const db = drizzle(process.env.DATABASE_URL!, { schema });
